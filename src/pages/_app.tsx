765de7ea6cbd6a34c9a48af45fea057a49b7d5d5
Shreyas Chavan <shreyaschavan228a@gmail.com>
2023-05-08 12:04:57 +0530

    home link in navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,7 +27,7 @@ const Header = () => {
     const { isSignedIn } = useUser();
     return (
         <footer className="flex flex-row bg-slate-800 justify-between align-center w-full basis-6 items-stretch text-white font-mono h-10 p-2 border-b-2 border-black">
-            <h1>Chat App</h1>
+            <h1><Link href="/">Chat App</Link></h1>
             <div>
                 {isSignedIn && <SignOutButton />}
                 {!isSignedIn && <SignInButton />}
