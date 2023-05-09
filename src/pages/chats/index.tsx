e56aaf836ce88349b05be7736e01c52b165379d8
Shreyas Chavan <shreyaschavan228a@gmail.com>
2023-05-09 22:10:25 +0530

    chats are served on seperate route

--- src/pages/chats/index.tsx
+++ src/pages/chats/index.tsx
@@ -10,6 +10,42 @@ import MessageView from "~/components/MessageView";
 import { api } from "~/utils/api";
 
 
+const MainContainer = () => {
+    const router = useRouter();
+    const id = router.asPath.slice(1);
+    const { user, isSignedIn, isLoaded } = useUser();
+    const [currentChat, setChat] = useState("");
+
+    // this will never load unless the user directly navigates to this specific url without signing in
+    if (!user) return <div>Something went wrong</div>
+
+
+    return (
+        <div className="w-full flex flex-row h-full">
+            {isSignedIn &&
+                <>
+                    <div className="chats w-1/3"><ChatView userId={user.id} setChat={setChat} /></div>
+                    <div className="message-window w-2/3 bg-gradient-to-b from-[#2e026d] to-[#15162c] h-full">
+                        {
+                            currentChat === "" &&
+                            <div className="w-full h-full flex items-center justify-center">
+                                <p className="text-3xl text-gray-400">Select a chat or start new</p>
+                            </div>
+                        }
+                        {
+                            currentChat !== "" && <MessageView otherUserId={currentChat} userid={user.id} key={currentChat} />
+                        }
+                    </div>
+                </>
+            }
+            {
+                !isSignedIn && <div>Something went wrong</div>
+            }
+        </div>
+    )
+}
+
+
 const Home: NextPage = () => {
     const { user, isSignedIn, isLoaded } = useUser();
     const router = useRouter();
@@ -25,13 +61,6 @@ const Home: NextPage = () => {
         }
     }
 
-    if (!isLoaded) {
-        return (
-            <div className="h-full w-full flex flex-col justify-center items-center">
-                <p className="text-3xl text-gray-400">Loading</p>
-            </div>
-        )
-    }
     return (
         <>
             <Head>
@@ -41,15 +70,13 @@ const Home: NextPage = () => {
             </Head>
             <main className="h-full flex flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white overflow-y-scroll">
                 {isSignedIn && user &&
-                    <div className="h-full w-full flex flex-col justify-center items-center">
-                        <p className="text-3xl text-gray-400"><Link href="./chats/">Continue to Chats</Link></p>
-                    </div>
+                    <MainContainer />
                 }
                 {
                     !isSignedIn &&
                     <div className="h-full w-full flex flex-col justify-center items-center">
-                        <p className="text-3xl text-gray-400">Sign in with Google or Github</p>
-                        <SignInButton />
+                        <p className="text-3xl text-gray-400">Not Signed In</p>
+                        <Link href=".." className="text-2xl underline">Home</Link>
                     </div>
                 }
             </main>
